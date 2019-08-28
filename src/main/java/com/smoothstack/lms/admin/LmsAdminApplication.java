98e49f45abc532ac683f8e47c84f0b256025274a
Allen-C-Chen <52220585+Allen-C-Chen@users.noreply.github.com>
2019-08-27 22:43:50 -0400

    Update LmsAdminApplication.java

--- src/main/java/com/smoothstack/lms/admin/LmsAdminApplication.java
+++ src/main/java/com/smoothstack/lms/admin/LmsAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LmsAdminApplication {
-
+	//test 1
 	public static void main(String[] args) {
 		SpringApplication.run(LmsAdminApplication.class, args);
 	}
